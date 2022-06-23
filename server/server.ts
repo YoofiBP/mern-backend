8a018abfbc52d5fe24df40d0d22cebacb3c92725
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2022-06-23 22:18:58 +0000

    Remove commented code

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,6 @@
 import app from "./express";
 import config from './config/config'
 
-// devBundle.compile(app);
-/*mongoose.connect(config.mongoUri);
-
-mongoose.connection.on('connected', () => {
-    console.info('Database connection established')
-});
-
-mongoose.connection.on('error', () => {
-    console.error('There was an error establishing the  database connection')
-})
-
-mongoose.connection.on('disconnected', () => {
-    console.info('Database connection lost');
-})*/
-
-
 app.listen(config.port, (err) => {
     if (err) {
         console.error(err);
